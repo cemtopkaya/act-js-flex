eb59e47430877dc4b982c0b6d88fb457b4df02b0
cemtopkaya <cem.topkaya@hotmail.com>
2019-03-30 23:05:24 +0300

    3 files modified

--- index.js
+++ index.js
@@ -5,68 +5,55 @@ import './css-animation.css';
 import './style.css';
 import $ from 'jquery';
 
-function sectionSpanCssChanged(sectionId = "section#flexWrap", cssProp = "flex-wrap") {
+function animateSpan(section, cssProp, value) {
+  let regexp = new RegExp(cssProp + ":(.*?;)"),
+    spans = section.querySelectorAll("pre span"),
+    span = Array.from(spans)
+      .find(span => span.innerText.indexOf(cssProp) > -1),
+    newWrap = span.innerText.replace(regexp, cssProp + ':' + value + ';'),
+    newCssValue = "newCssValue"
+
+  span.innerHTML = newWrap
+  if (span.classList.contains(newCssValue)) {
+    span.classList.toggle(newCssValue);
+  }
+
+  setTimeout(() => span.classList.add(newCssValue), 1)
+}
+
+function section_radioChanged(sectionId = "section#flexWrap", cssProp) {
   const section = document.querySelector(sectionId),
     divContainer = section.querySelector("div"),
     radios = section.querySelectorAll("input[type='radio']"),
     pre = section.querySelector("pre"),
+    cssProp = cssProp || radios[0].name,
     onChange = (evt) => {
-      
-console.log("span")
       divContainer.style[cssProp] = evt.target.value
-      let regexp = new RegExp(cssProp + ":(.*?;)"),
-        spans = section.querySelectorAll("pre span"),
-        span = Array.from(spans)
-          .find(span => span.innerText.indexOf(cssProp) > -1),
-        newWrap = span.innerText.replace(regexp, cssProp + ':' + evt.target.value + ';'),
-        newCssValue = "newCssValue"
-
-      span.innerHTML = newWrap
-      if (span.classList.contains(newCssValue)) {
-        span.classList.toggle(newCssValue);
-      }
-
-      setTimeout(() => span.classList.add(newCssValue), 1)
+      animateSpan(section, cssProp, evt.target.value)
     }
-console.log(radios)
+
+  console.log(radios)
   radios.forEach(r => {
     r.addEventListener('change', onChange)
   })
 }
 
+function section_rangeInput(sectionId = "section#flexGrowWrap") {
+  let section = document.querySelector(sectionId)
 
-function section_flexGrowWrap() {
-  const sectionIdFlexGrowWrap = "#flexGrowWrap";
-  let flexGrowSlider,
-    sectionFlexGrow,
-    flexGrow_wrap_RGs;
-
-  sectionFlexGrow = document.querySelector(sectionIdFlexGrowWrap);
-  if (sectionFlexGrow) {
-
-    flexGrowSlider = sectionFlexGrow.querySelector("input[type='range']");
-    flexGrow_wrap_RGs = sectionFlexGrow.querySelectorAll("input[type='radio']");
-
-    if (flexGrow_wrap_RGs) {
-      let fnWrapOnChange = function (evt) {
-        let divContainer = document.querySelector(sectionIdFlexGrowWrap + ">div");
-        divContainer.style["flex-wrap"] = evt.target.value;
-        sectionSpanCssChanged("flexGrowWrap", "flex-wrap");
-      };
-
-      flexGrow_wrap_RGs.forEach(r => {
-        r.addEventListener('change', fnWrapOnChange, false)
-      })
-    }
+  if (section) {
+    let
+      flexGrowSlider = section.querySelector("input[type='range']"),
+      divContainer = section.querySelector("div"),
+      divs = divContainer.querySelectorAll("div");
 
     if (flexGrowSlider) {
 
       let onInput = function () {
-        var div = sectionFlexGrow.querySelector("div");
-        div.style.width = flexGrowSlider.value / 10 + "%";
-        sectionSpanCssChanged("flexGrowWrap", "width");
+        const newWidth = flexGrowSlider.value / 10 + "%";
+        divContainer.style.width = newWidth;
+        animateSpan(section, "width", newWidth);
 
-        var divs = document.querySelectorAll(sectionIdFlexGrowWrap + ">div>div");
         divs.forEach(div => {
           let span = div.getElementsByTagName("span")[0];
           span.innerHTML = "<br/>" + div.offsetWidth + "px"
@@ -80,23 +67,11 @@ function section_flexGrowWrap() {
     }
   }
 }
-// section_flexGrowWrap()
 
-function section_flexWrap() {
 
-  const section = document.querySelector("section#flexWrap"),
-    flexWrap_radios = section.querySelectorAll("input[type='radio']"),
-    fnWrapOnChange = function (evt) {
-      let divContainer = section.querySelector("div");
-      divContainer.style["flex-wrap"] = evt.target.value;
-      sectionSpanCssChanged("section#flexGrowWrap", "flex-wrap");
-    };
-
-  flexWrap_radios.forEach(r => {
-    r.addEventListener('change', fnWrapOnChange, false)
-  })
-}
-//section_flexWrap()
-
-sectionSpanCssChanged("section#flexWrap", "flex-wrap");
-sectionSpanCssChanged("section#flexGrowWrap", "flex-wrap");
+(function () {
+  section_rangeInput("section#flexGrowWrap")
+  section_radioChanged("section#flexWrap", "flex-wrap");
+  section_radioChanged("section#flexGrowWrap", "flex-wrap");
+  section_radioChanged("section#justifyContent");
+})();
